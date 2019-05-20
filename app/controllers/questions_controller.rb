4aa7d58d380a06e182bf345686f7a806829e3ac7
Louis Paulet <louis.paulet.14@neoma-bs.com>
2019-05-20 13:45:18 +0200

    Ex Rails Stupid Coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = params[:question]
+    if @answer.include?('?')
+      @coach_answer = 'Silly question, get dressed and go to work!'
+    elsif @answer == 'I am going to work right now!'
+      @coach_answer = 'Great!'
+    else
+      @coach_answer = "I don't care, get dressed and go to work"
+    end
+  end
+end
